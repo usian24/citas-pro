195693820ca5b064b78dbd0c74a5ac770ee19148
lucian aguilar <aguilarquispejhonalberto@gmail.com>
2026-04-17 21:31:45 -0500

    v2

--- assets/js/client-portal.js
+++ assets/js/client-portal.js
@@ -73,24 +73,24 @@ function loadBizDirect(bizId) {
   var socialsHtml = '';
   
   if (biz.insta) {
-    // Limpia: http, https, www, instagram.com, arrobas y barras iniciales
-    var cleanInsta = biz.insta.replace(/^(https?:\/\/)?(www\.)?instagram\.com\/?/i, '').replace(/[@/]/g, '').trim();
+    var rawInsta = biz.insta.split('?')[0];
+    var cleanInsta = rawInsta.replace(/(https?:)?(\/\/)?(www\.)?instagram\.com\/?/i, '').replace(/[@/]/g, '').trim();
     var igUrl = 'https://www.instagram.com/' + cleanInsta;
     
     socialsHtml += '<a href="' + igUrl + '" target="_blank" style="color:var(--blue);transition:opacity 0.2s;" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg></a>';
   }
   
   if (biz.facebook) {
-    // Limpia: http, https, www, facebook.com y barras iniciales
-    var cleanFb = biz.facebook.replace(/^(https?:\/\/)?(www\.)?facebook\.com\/?/i, '').replace(/^\//, '').trim();
+    var rawFb = biz.facebook.split('?')[0];
+    var cleanFb = rawFb.replace(/(https?:)?(\/\/)?(www\.)?facebook\.com\/?/i, '').replace(/^\//, '').trim();
     var fbUrl = 'https://www.facebook.com/' + cleanFb;
     
     socialsHtml += '<a href="' + fbUrl + '" target="_blank" style="color:var(--blue);transition:opacity 0.2s;" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg></a>';
   }
   
   if (biz.x_url) {
-    // Limpia: http, https, www, x.com, twitter.com, arrobas y barras iniciales
-    var cleanX = biz.x_url.replace(/^(https?:\/\/)?(www\.)?(x\.com|twitter\.com)\/?/i, '').replace(/[@/]/g, '').trim();
+    var rawX = biz.x_url.split('?')[0];
+    var cleanX = rawX.replace(/(https?:)?(\/\/)?(www\.)?(x\.com|twitter\.com)\/?/i, '').replace(/[@/]/g, '').trim();
     var xUrl = 'https://x.com/' + cleanX;
     
     socialsHtml += '<a href="' + xUrl + '" target="_blank" style="color:var(--blue);transition:opacity 0.2s;" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4l11.733 16h4.267l-11.733-16z"></path><path d="M4 20l6.768-6.768m2.46-2.46l6.772-6.772"></path></svg></a>';
