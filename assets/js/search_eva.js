679c61cd23cb964b4fcdf78761339b9267573565
lucian aguilar <aguilarquispejhonalberto@gmail.com>
2026-04-17 21:12:34 -0500

    corrigiendo las rutas de las redes sociales

--- assets/js/search_eva.js
+++ assets/js/search_eva.js
@@ -120,17 +120,26 @@
     var socialsHtml = '';
     
     if (biz.insta) {
-      var igUrl = biz.insta.startsWith('http') ? biz.insta : 'https://instagram.com/' + biz.insta.replace('@', '');
+      var cleanInsta = biz.insta.replace(/^(https?:\/\/)?(www\.)?instagram\.com\/?/i, '').replace(/[@/]/g, '').trim();
+      var igUrl = 'https://www.instagram.com/' + cleanInsta;
+      
       socialsHtml += '<a href="' + sanitizeHtml(igUrl) + '" target="_blank" style="color:var(--text); transition:opacity 0.2s" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg></a>';
     }
+    
     if (biz.facebook) {
-      var fbUrl = biz.facebook.startsWith('http') ? biz.facebook : 'https://facebook.com/' + biz.facebook;
+      var cleanFb = biz.facebook.replace(/^(https?:\/\/)?(www\.)?facebook\.com\/?/i, '').replace(/^\//, '').trim();
+      var fbUrl = 'https://www.facebook.com/' + cleanFb;
+      
       socialsHtml += '<a href="' + sanitizeHtml(fbUrl) + '" target="_blank" style="color:var(--text); transition:opacity 0.2s" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg></a>';
     }
+    
     if (biz.x_url) {
-      var xUrl = biz.x_url.startsWith('http') ? biz.x_url : 'https://x.com/' + biz.x_url.replace('@', '');
+      var cleanX = biz.x_url.replace(/^(https?:\/\/)?(www\.)?(x\.com|twitter\.com)\/?/i, '').replace(/[@/]/g, '').trim();
+      var xUrl = 'https://x.com/' + cleanX;
+      
       socialsHtml += '<a href="' + sanitizeHtml(xUrl) + '" target="_blank" style="color:var(--text); transition:opacity 0.2s" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1"><svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4l11.733 16h4.267l-11.733 -16z"></path><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path></svg></a>';
     }
+    
     socialsContainer.innerHTML = socialsHtml;
 
     document.getElementById('bp-btn-book').onclick = function() {
