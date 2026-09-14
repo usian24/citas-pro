37f41cbbcb0912c7cef36675e79ee279dc3106be
lucian aguilar <aguilarquispejhonalberto@gmail.com>
2026-09-13 22:07:15 -0500

    compresor de imagenes

--- assets/js/image-engine.js
+++ assets/js/image-engine.js
@@ -26,7 +26,7 @@ async function processImageForUpload(file) {
   };
 
   try {
-    const compressedFile = await imageCompression(file, options);
+    const compressedFile = await browserImageCompression(file, options);
     return compressedFile;
   } catch (error) {
     console.error('Error al optimizar la imagen, se usará la original:', error);
